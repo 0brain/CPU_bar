f026e4210f9bcdba19d3788c573d45d0cad5f4bd
nazar <nazar.khort@gmail.com>
2021-12-14 00:08:45 +0200

    Banned displaying a frame and opportunity to change the size of the window.

--- CPU_bar.py
+++ CPU_bar.py
@@ -4,8 +4,10 @@ import tkinter as tk
 class Application(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        self.attributes('-alpha', 1) # 100 percent transparency
-        self.attributes('-topmost', True) # window on top of other windows
+        self.attributes('-alpha', 1)  # 100 percent transparency
+        self.attributes('-topmost', True)  # window on top of other windows
+        self.overrideredirect(True)  # ban the frame
+        self.resizable(False, False)  # ban alter the size of the window
 
 
 root = Application()

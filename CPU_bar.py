5bce48948c9c0e0d4cf39f857d29abe066b45c92
nazar <nazar.khort@gmail.com>
2021-12-13 23:59:45 +0200

    Added 2 attrs: 100 percent transparency and window on top of other windows

--- CPU_bar.py
+++ CPU_bar.py
@@ -4,6 +4,8 @@ import tkinter as tk
 class Application(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+        self.attributes('-alpha', 1) # 100 percent transparency
+        self.attributes('-topmost', True) # window on top of other windows
 
 
 root = Application()

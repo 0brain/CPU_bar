e6db6bad1301393af3be1380c4a6b2f05e85149d
nazar <nazar.khort@gmail.com>
2021-12-15 15:44:01 +0200

    Changed transparency construction because it is not work on ubuntu. Added UI of exit button and function which allows to close app.

--- CPU_bar.py
+++ CPU_bar.py
@@ -1,14 +1,28 @@
 import tkinter as tk
+from tkinter import ttk
+import sys
 
 
 class Application(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
-        self.attributes('-alpha', 1)  # 100 percent transparency
+        #self.attributes('-alpha', 1)  # 100 percent transparency
+        self.wait_visibility()
+        self.wm_attributes('-alpha', 1)
         self.attributes('-topmost', True)  # window on top of other windows
         self.overrideredirect(True)  # ban the frame
         self.resizable(False, False)  # ban alter the size of the window
         self.title('CPU bar')
+
+        self.set_ui()
+
+    def set_ui(self):
+        exit_but = ttk.Button(self, text='Exit', command=self.app_exit)
+        exit_but.pack(fill=tk.X)
+
+    def app_exit(self):
+        self.destroy()
+        sys.exit()
 
 
 root = Application()

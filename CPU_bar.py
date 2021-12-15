b34206939e1ee5471bfcb0a48fd8ab9ff1ecdc03
nazar <nazar.khort@gmail.com>
2021-12-15 16:32:02 +0200

    Added 2 buttons and menu-selector. Just added, without some logic connection.

--- CPU_bar.py
+++ CPU_bar.py
@@ -8,7 +8,7 @@ class Application(tk.Tk):
         tk.Tk.__init__(self)
         #self.attributes('-alpha', 1)  # 100 percent transparency
         self.wait_visibility()
-        self.wm_attributes('-alpha', 1)
+        self.attributes('-alpha', 1)
         self.attributes('-topmost', True)  # window on top of other windows
         self.overrideredirect(True)  # ban the frame
         self.resizable(False, False)  # ban alter the size of the window
@@ -19,6 +19,19 @@ class Application(tk.Tk):
     def set_ui(self):
         exit_but = ttk.Button(self, text='Exit', command=self.app_exit)
         exit_but.pack(fill=tk.X)
+
+        self.bar2 = ttk.LabelFrame(self, text='Manual')
+        self.bar2.pack(fill=tk.X)
+
+        self.combo_win = ttk.Combobox(self.bar2,
+                                      values=["hide", "don't hide", 'min'],
+                                      width=9, state='readonly')
+
+        self.combo_win.current(1)
+        self.combo_win.pack(side=tk.LEFT)
+
+        ttk.Button(self.bar2, text='move').pack(side=tk.LEFT)
+        ttk.Button(self.bar2, text='>>>').pack(side=tk.LEFT)
 
     def app_exit(self):
         self.destroy()
